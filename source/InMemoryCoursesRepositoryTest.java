946ff39d5e67dfbb7c3898b4afe415fe9df8ed02
AssoDePicche <samuelprado730@gmail.com>
2025-08-19 14:13:32 -0300

    test: InMemoryCoursesRepositoryTest.java

---
+++ source/InMemoryCoursesRepositoryTest.java
@@ -0,0 +1,8 @@
+public final class InMemoryCoursesRepositoryTest extends TestCase {
+  private CoursesRepository repository = new InMemoryCoursesRepository();
+
+  @Test
+  public void queryCoursesByDifficultyLevelShouldReturnInDescendingOrder() {
+    // TODO: test use case
+  }
+}
