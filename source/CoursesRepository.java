d980d0bd1618e5004fafde2b1ed22540d923498d
AssoDePicche <samuelprado730@gmail.com>
2025-08-19 20:33:25 -0300

    feat: query course by title

--- source/CoursesRepository.java
+++ source/CoursesRepository.java
@@ -1,9 +1,12 @@
+import java.util.Optional;
 import java.util.Set;
 
 public interface CoursesRepository {
   Set<Course> query();
 
   Set<Course> query(DifficultyLevel difficultyLevel);
+
+  Optional<Course> query(String title);
 
   void save(Course course);
 }
