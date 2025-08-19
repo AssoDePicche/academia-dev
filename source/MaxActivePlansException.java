d8e3b68f7af13f3fad188bddc1e300955a67fea4
AssoDePicche <samuelprado730@gmail.com>
2025-08-19 14:09:39 -0300

    refactor: rename MaxActivePlansException -> MaxOngoingCoursesException

--- source/MaxActivePlansException.java
+++
@@ -1,6 +0,0 @@
-@SuppressWarnings({"serial"})
-public final class MaxActivePlansException extends RuntimeException {
-  public MaxActivePlansException(String message) {
-    super(message);
-  }
-}
---
+++ source/MaxOngoingCoursesException.java
@@ -0,0 +1,6 @@
+@SuppressWarnings({"serial"})
+public final class MaxOngoingCoursesException extends RuntimeException {
+  public MaxOngoingCoursesException(String message) {
+    super(message);
+  }
+}
